4944fe148462bd2f01af1b0b5998036a8de19285
maheshbasnet089 <basnetmanish089@gmail.com>
2023-09-15 21:41:58 +0545

    add db

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,8 +1,25 @@
-module.exports = {
-    HOST: "localhost",
+// FOR LOCAL ENVIRONMENT
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "root",
+//     PASSWORD: "",
+//     DB: "sepcms",
+//     dialect: "mysql",
+//     pool: {
+//       max: 5,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000,
+//     },
+//   };
+
+
+// for production 
+  module.exports = {
+    HOST: "containers-us-west-206.railway.app",
     USER: "root",
-    PASSWORD: "",
-    DB: "sepcms",
+    PASSWORD: "qz3ncGhNgAyUlzqrvo8o",
+    DB: "railway",
     dialect: "mysql",
     pool: {
       max: 5,
